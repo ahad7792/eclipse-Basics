1a4ffa2ea984355b7b03afab4f08913d2dc84f6e
ahad7792 <ahad7792@gmail.com>
2022-09-17 19:43:28 +0600

    commit

--- Data structures and Algorithms/src/com/ahad/leetcode/RotateArray.java
+++ Data structures and Algorithms/src/com/ahad/leetcode/RotateArray.java
@@ -17,5 +17,22 @@ public class RotateArray {
 		}
 		nums[0]  = temp; //putting the temp at the first index
 	}
+	
+	public void rotateOptimized(int[] nums, int k) {
+		int l = k % nums.length; //checking if the k is bigger than the nums.length
+		reverse(nums, 0, nums.length - 1);
+		reverse(nums, 0, l - 1);
+		reverse(nums, l, nums.length - 1);
+	}
+
+	private void reverse(int[] nums, int i, int j) {
+		while(i < j) {
+			int temp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = temp;
+			i++;
+			j--;
+		}
+	}
 
 }
