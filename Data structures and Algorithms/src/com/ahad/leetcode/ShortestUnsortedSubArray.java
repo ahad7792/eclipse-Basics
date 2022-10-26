9cf4065a10014464d55aaf92c3b1b4ce3e9fc8c7
ahad7792 <ahad7792@gmail.com>
2022-10-27 01:07:34 +0600

    commit

--- Data structures and Algorithms/src/com/ahad/leetcode/ShortestUnsortedSubArray.java
+++ Data structures and Algorithms/src/com/ahad/leetcode/ShortestUnsortedSubArray.java
@@ -5,21 +5,21 @@ public class ShortestUnsortedSubArray {
 	public int findUnsortedSubarray(int[] nums) {
 		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 
-		for (int i = 1; i < nums.length; i++) {
+		for (int i = 1; i < nums.length; i++) { //finding which minimum value broke the order
 			if (nums[i] < nums[i - 1])
 				min = Math.min(min, nums[i]);
 		}
 
-		for (int i = nums.length - 2; i >= 0; i--) {
+		for (int i = nums.length - 2; i >= 0; i--) { //finding which maximum value broke the order
 			if (nums[i] > nums[i + 1])
 				max = Math.max(max, nums[i]);
 		}
 		int l, r;
-		for (l = 0; l < nums.length; l++) {
+		for (l = 0; l < nums.length; l++) { //finding the position for minimum value
 			if (min < nums[l])
 				break;
 		}
-		for (r = nums.length - 1; r >= 0; r--) {
+		for (r = nums.length - 1; r >= 0; r--) { //finding the position for maximum value
 			if (max > nums[r])
 				break;
 		}
